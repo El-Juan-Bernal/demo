67927c260e6545311c4f7fd3c1c1d168d8e7accd
El-Juan-Bernal <ju.bernala@duocuc.cl>
2025-04-30 12:37:43 -0400

    first commit

---
+++ demo/src/main/java/com/PorTerceraVez/demo/MascotassApplication.java
@@ -0,0 +1,13 @@
+package com.PorTerceraVez.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotassApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/PorTerceraVez/demo/MascotassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PorTerceraVez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
