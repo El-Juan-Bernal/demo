ecc6d01b1f094ae53412d0ce7edac7d931639584
El-Juan-Bernal <ju.bernala@duocuc.cl>
2025-04-30 13:10:12 -0400

    guardar

--- demo/src/main/java/com/PorTerceraVez/demo/controller/MascotasController.java
+++ demo/src/main/java/com/PorTerceraVez/demo/controller/MascotasController.java
@@ -2,10 +2,10 @@ package com.PorTerceraVez.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.annotation.Resource;
 
-@Resource
+@RestController
 @RequestMapping("/api/mascotas")
 
 public class MascotasController {
